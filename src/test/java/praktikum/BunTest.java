8993cf3059ef31a64d358bce1a1c8a55f0c5abb2
Ivan Krutikov <infr4rot@gmail.com>
2023-11-06 00:05:51 +0700

    some CR

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -3,22 +3,22 @@ package praktikum;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import praktikum.constants.NameConstant;
 
 public class BunTest {
 
-    private final String NAME = "Test Name";
     private final Float PRICE = 1.55f;
     private Bun bun;
 
     @Before
     public void init() {
-        bun = new Bun(NAME, PRICE);
+        bun = new Bun(NameConstant.BUN_NAME.getName(), PRICE);
     }
 
     @Test
     public void shouldReturnName() {
         String actual = bun.getName();
-        Assert.assertEquals("Возвращаемое название не соответствует ожидаемому",NAME,actual);
+        Assert.assertEquals("Возвращаемое название не соответствует ожидаемому",NameConstant.BUN_NAME.getName(),actual);
     }
 
     @Test
